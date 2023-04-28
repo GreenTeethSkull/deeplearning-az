013067cce5b0b8337575a0034af59964a1eed881
GreenTeethSkull <angelitorios366@gmail.com>
2023-04-27 22:00:48 -0500

    my cnn updates

--- datasets/Part 2 - Convolutional Neural Networks (CNN)/cnn.py
+++ datasets/Part 2 - Convolutional Neural Networks (CNN)/cnn.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr 29 19:20:59 2019
+Created on 2023
 
-@author: juangabriel
+@author: greenskull
 """
-
 # Redes Neuronales Convolucionales
-
-# Instalar Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Instalar Tensorflow y Keras
-# conda install -c conda-forge keras
 
 
 # Parte 1 - Construir el modelo de CNN
@@ -70,18 +63,21 @@ testing_dataset = test_datagen.flow_from_directory('dataset/test_set',
                                                 batch_size=32,
                                                 class_mode='binary')
 
-classifier.fit_generator(training_dataset,
-                        steps_per_epoch=8000,
-                        epochs=25,
-                        validation_data=testing_dataset,
-                        validation_steps=2000)
+
+classifier.fit(training_dataset,
+               steps_per_epoch=int(8000/32), # num_imgs / batch_size
+               epochs=25,
+               validation_data=testing_dataset,
+               validation_steps=int(2000/32) # num_imgs / batch_size
+               )
 
 
 # Parte 3 - Cómo hacer nuevas predicciones
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
-test_image = image.img_to_array(test_image)
+from keras import utils
+test_image = utils.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = utils.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
 training_dataset.class_indices
@@ -89,6 +85,37 @@ if result[0][0] == 1:
     prediction = 'dog'
 else:
     prediction = 'cat'
+print(prediction)
+    
+    
+    
+    
+    
+
+# MLP for Pima Indians Dataset Serialize to JSON and HDF5
+from keras.models import Sequential, model_from_json
+    
+# serialize model to JSON
+model_json = classifier.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+classifier.save_weights("model.h5")
+print("Saved model to disk")
+ 
+# later...
+ 
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+ 
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
 
 
